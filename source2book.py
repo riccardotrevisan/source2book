c572fb40061fc57bf03bb5e4050cd33d02c7b4f0
riccardotrevisan <riccardo.trevisan+github@gmail.com>
2019-12-06 18:29:03 +0100

    Added source files header removal

--- source2book.py
+++ source2book.py
@@ -8,29 +8,42 @@ import os
 
 class SourceFile(str):
     """
-    Class that define a source file to be processed
+    Class that defines a source file to be processed
     """
-    def __init__(self, filename):
-        str.__init__(filename)
-        self.extension = os.path.splitext(filename)[1][1:]
+    def __init__(self, filepath):
+        str.__init__(self)
+        self.filepath = filepath
+        self.filename = os.path.basename(filepath)
+        self.extension = os.path.splitext(self.filename)[1][1:]
+        with open(filepath, 'r') as f:
+            self.content = f.read()
+
+    def remove_header(self, nrows):
+        self.content = ''.join(self.content.splitlines()[nrows:])
 
 
 def main():
     """
     Main program
     """
+    # Read globals
     with open('config.yaml') as f:
         options = load(f, Loader=BaseLoader)
     sourcepath = options['sourcefolder']
     subfolders = options['subfolders']
     extensions = options['filter']
+    # Compose file list
     for subfolder in subfolders:
-        print('Chapter ' + subfolder)
-        sourcefilelist = [SourceFile(entry.name)
+        # print('Chapter ' + subfolder)
+        sourcefilelist = [SourceFile(entry.path)
                           for entry in os.scandir(os.path.join(sourcepath, subfolder))
                           if os.path.splitext(entry.name)[1][1:] in extensions]
         sourcefilelist.sort()
-        print(sourcefilelist)
+        # Remove header
+        headingrows = int(options['headingrows'])
+        for sourcefile in sourcefilelist:
+            sourcefile.remove_header(headingrows)
+            print('Processed file: ' + sourcefile)
 
 
 if __name__ == '__main__':
