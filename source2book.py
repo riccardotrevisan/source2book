d3262ba1535ff24c571a403731b46be50c699b93
riccardotrevisan <riccardo.trevisan+github@gmail.com>
2019-12-07 09:19:26 +0100

    Added html page compose

--- source2book.py
+++ source2book.py
@@ -17,9 +17,36 @@ class SourceFile(str):
         self.extension = os.path.splitext(self.filename)[1][1:]
         with open(filepath, 'r') as f:
             self.content = f.read()
+        self.htmlcode = ''
 
     def remove_header(self, nrows):
-        self.content = ''.join(self.content.splitlines()[nrows:])
+        self.content = ''.join(self.content.splitlines(keepends=True)[nrows:])
+
+    def add_html_chapter(self):
+        self.htmlcode += '<h2>\n' + self.filename + '\n</h2>\n'
+
+    def add_html_body(self):
+        self.htmlcode += '<pre>\n' + \
+                         self.content.replace('&', '&amp;').\
+                             replace('>', '&gt;').\
+                             replace('<', '&lt;') + '</pre>\n'
+
+    def compose_html(self):
+        self.add_html_chapter()
+        self.add_html_body()
+        pass
+
+    @staticmethod
+    def add_html_header_footer(title, html_body):
+        header = "<!DOCTYPE html>\n" \
+                 "<html>\n" \
+                 "<head><title>" + \
+                 title + \
+                 "</title></head>" \
+                 "<body>\n"
+        footer = "\n</body>\n" \
+                 "</html>\n"
+        return header + html_body + footer
 
 
 def main():
@@ -33,17 +60,23 @@ def main():
     subfolders = options['subfolders']
     extensions = options['filter']
     # Compose file list
+    html_page_body = ''
     for subfolder in subfolders:
-        # print('Chapter ' + subfolder)
         sourcefilelist = [SourceFile(entry.path)
                           for entry in os.scandir(os.path.join(sourcepath, subfolder))
                           if os.path.splitext(entry.name)[1][1:] in extensions]
         sourcefilelist.sort()
         # Remove header
+        html_page_body += '<h1>Chapter ' + subfolder + '</h1>\n'
         headingrows = int(options['headingrows'])
         for sourcefile in sourcefilelist:
             sourcefile.remove_header(headingrows)
+            sourcefile.compose_html()
+            html_page_body += sourcefile.htmlcode
             print('Processed file: ' + sourcefile)
+    html_page_content = SourceFile.add_html_header_footer(options['title'], html_page_body)
+    with open('output.html', 'w') as f:
+        f.write(html_page_content)
 
 
 if __name__ == '__main__':
